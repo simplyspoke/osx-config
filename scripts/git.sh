67d3ad197f9f4dafccdd1d50604ffd54aac063da
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-12-31 12:29:31 -0600

    Updated git configurations

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo '\nSetup Git Configurations\n'
-cp -f ../files/.gitconfig ~/.gitconfig
-cp -f ../files/.gitignore ~/.gitignore
+cp -f ./files/.gitconfig ~/.gitconfig
+cp -f ./files/.gitignore ~/.gitignore
 
 git config --global user.name $fullName
 git config --global user.email $email
 git config --global core.excludesfile ~/.gitignore
 
-read -p "\nWould you like to configure Github? (y/n)\n" github
+echo '\nWould you like to configure Github? (y/n)\n'
+read github
 
 if [ "$github" = "y" ]; then
   echo "Please enter your Github username: "
