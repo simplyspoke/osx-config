9aa8c67da05a642e6077ef78fe4c106ef6da678d
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-12-31 12:33:09 -0600

    Added a script to install some UI applications

---
+++ scripts/ui-applications.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo '\nInstall Homebrew\n'
+
+
+echo '\nInstall Brew Applications\n'
+brew update --force
+brew bundle --file=- <<EOF
+tap "caskroom/cask"
+
+cask 'alfred'
+cask 'appcleaner'
+cask 'bitbar'
+cask 'cakebrew'
+cask 'citrix-receiver'
+cask 'dash'
+cask 'day-o'
+cask 'docker-toolbox'
+cask 'dropbox'
+cask 'flux'
+cask 'gimp'
+cask 'google-chrome'
+cask 'insomniax'
+cask 'iterm2'
+cask 'launchcontrol'
+cask 'macpass'
+cask 'robo-3t'
+cask 'scroll-reverser'
+cask 'skype-for-business'
+cask 'slack'
+cask 'spectacle'
+cask 'spotify'
+cask 'textmate'
+cask 'time-out'
+cask 'tomighty'
+
+mas "Blogo", id: 902111700
+mas "Flow ai", id: 1290204939
+mas "Harvest", id: 506189836
+EOF
